2fb0f56a0da3797c47ad598c6ea2bef12764d3de
Anuj Gill <anujgill212@gmail.com>
2024-03-07 10:59:45 +0530

    Basic UI for all components completed v3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,25 +6,25 @@ export function Home({ socket }) {
   console.log(socket.id);
 
   return (
-    <div className="flex flex-col items-center justify-around min-h-screen">
-      <div className="mb-4">
-        <h1 className="mt-10 text-xl md:text-4xl lg:text-5xl xl:text-6xl font-bold font-irish-grover text-white text-center items-center">
-          Doodle Wars
-        </h1>
+    <div className="flex flex-col items-center justify-around  min-h-screen">
+      <div className=" mb-4">
+        <h1 className="mt-10 text-8xl font-bold font-irish-grover text-white  sm:text-6xl md:text-8xl">Doodle Wars</h1>
       </div>
-      <div className="flex flex-col gap-4 mb-4">
+      <div className="flex flex-col sm:flex-row mb-4 gap-x-20 ">
         <button
-          className="w-full max-w-xs px-6 py-3 bg-white text-blue-900 rounded-md font-irish-grover font-bold hover:text-white hover:bg-blue-900 transition-colors duration-300"
+          className="px-6 py-3 mb-4 sm:mb-0 sm:mr-4 bg-white font-irish-grover text-blue-900 rounded-md hover:text-white hover:bg-blue-900 transition-colors duration-300 font-bold "
           onClick={() => navigate('/createroom')}
         >
           CREATE ROOM
         </button>
         <button
-          className="w-full max-w-xs px-6 py-3 bg-white text-blue-900 rounded-md font-irish-grover font-bold hover:text-white hover:bg-blue-900 transition-colors duration-300"
+          className="px-6 py-3 font-irish-grover font-bold bg-white text-blue-900 rounded-md hover:text-white hover:bg-blue-900 transition-colors duration-300"
           onClick={() => navigate('/joinroom')}
         >
           JOIN ROOM
         </button>
+      </div>
+      <div className="flex items-center">
       </div>
     </div>
   );
